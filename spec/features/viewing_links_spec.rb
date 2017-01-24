de758beb3bee75f9e34ca8c3df0960937385f78f
PeteGH <peter@grant-hay.eu>
2017-01-24 18:18:29 +0100

    adding link model

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,15 +1,11 @@
 require 'spec_helper'
 
+
 feature 'Viewing links' do
 
   scenario 'I can see existing links on the links page' do
-    # We can use `.create`, which we used in irb to make a Student, within our test!
-    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
-
+    Link.create(url: 'https://github.com/makersacademy/course/blob/master/course_outline.md', title: 'Makers Academy course outline')
     visit '/links'
-
-    # this is a *temporary* sanity check - to make sure we
-    # can load the page :)
     expect(page.status_code).to eq 200
 
     within 'ul#links' do

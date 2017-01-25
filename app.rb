26451f0b89a05560e2cf0a9da461871dec08bd47
PeteGH <peter@grant-hay.eu>
2017-01-25 12:37:51 +0100

    adding modular sinatra ch10

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
+require './models/link'
 require 'sinatra/base'
 require 'rspec'
 
 class BMM < Sinatra::Base
+
   get '/' do
     'Hello BMM!'
+    redirect '/links'
+  end
+
+  get '/links' do # adding links path
+    @links = Link.all # using DataMapper to get all the Link ojects
+    erb :'links/index' # d
   end
 
   # start the server if ruby file executed directly
